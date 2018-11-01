bdc9142f4ecb36db5943f658cb13be94104caa22
Anatooliy <Anatooliy@ukr.net>
2018-11-01 13:23:14 +0200

    first commit

---
+++ Requests/BusinessModels/Writer.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Web;
+
+namespace Requests.BusinessModels
+{
+    public class Writer
+    {
+        private readonly string requestType;
+        private readonly string ip;
+        private readonly string url;
+
+        public Writer(string requestType, string ip, string url)
+        {
+            this.requestType = requestType;
+            this.ip = ip;
+            this.url = url;
+        }
+
+        public void WriteToFile()
+        {
+            string path = HttpContext.Current.Server.MapPath("~/Content/RequestFiles/") + requestType + ".txt";
+            string requestInfo = "\nrequestType: " + requestType + "\n" + "ip: " + ip + "\n" + "url: " + url;
+
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(requestInfo);
+            }
+        }           
+    }
+}
---
+++ Requests/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Requests.BusinessModels;
+
+namespace Requests.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       // [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        public ActionResult RequestWriteToFile()
+        {
+            string requestType = Request.RequestType;
+            string ip = Request.UserHostAddress; 
+            string url = Request.RawUrl;
+
+            Writer writer = new Writer(requestType, ip, url);
+            writer.WriteToFile();
+
+            return View("Index");
+        }
+       
+    }
+}
